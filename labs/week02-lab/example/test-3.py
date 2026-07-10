3392680e57d2c81a0babbf6d9344abe5198adefc
jakkrit fasanthia <Jakkrit.f@ku.th>
2026-07-10 10:05:18 +0700

    kiti

---
+++ labs/week02-lab/example/test-3.py
@@ -0,0 +1,14 @@
+print("4. BMI Calculator:")
+print("   - Ask for weight (kg) and height (m)")
+print("   - Calculate: BMI = weight / (height ** 2)")
+print()
+
+#input
+weight = float(input("wight (kg): "))
+hwight = foat(input("Hwight "m": "))
+
+#process
+bmi = weight / (height ** 2)
+
+#output
+print("BMI =", bmi)
